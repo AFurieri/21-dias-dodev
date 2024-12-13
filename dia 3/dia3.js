a56a252e8edd6f026a5d20b93aa775d711122358
Arthur furieri <art.furieri@gmail.com>
2024-12-13 12:08:48 -0300

    projeto do dia 3

---
+++ dia 3/dia3.js
@@ -0,0 +1,7 @@
+const nome = prompt("insira seu nome")
+const idade = parseInt(prompt("insira sua idade"))
+const altura = Number(prompt("insira sua altura"))
+const peso = parseInt(prompt("insira seu peso"))
+const nascimento = (2023 - idade)
+const imc = peso/(altura*altura)
+    console.log("olá " + nome + ", você tem " + idade + " anos, nasceu em " + nascimento + ", tem " + altura + " de altura, pesa " + peso + "kg, e seu IMC é " + imc + " kg/m2")
