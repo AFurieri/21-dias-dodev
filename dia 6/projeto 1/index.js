26ab907368154b44e065ee2ea2f23ffc6cba937f
Arthur furieri <art.furieri@gmail.com>
2024-12-25 21:50:25 -0300

    projetos do dia 6

--- dia 6/projeto 1/index.js
+++ dia 6/projeto 1/index.js
@@ -1,4 +1,4 @@
 const num = parseInt(prompt("insira um número positivo e inteiro"))
 for(let i = 0; i<=num; i++){
-    console.log(i)
+     console.log(i)
 }
--- dia 6/projeto 3/index.js
+++ dia 6/projeto 3/index.js
@@ -1,3 +1,3 @@
 for(let i = 50; i >= 0; i -= 5){
-    console.log(i)
+     console.log(i)
 }
